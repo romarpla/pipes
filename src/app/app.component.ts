334919e1f40f166f4650433a6dc50a5bd3423d82
RODRIGO\r <jrodrigo.martinez@hotmail.com>
2018-01-05 12:14:16 -0600

    slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  nombre:string = 'Rodrigo';
+  arreglo:number [] = [1,2,3,4,5,6,7,8,9,10];
 }
