04958775e2c583f8d15efea41744189d93e49a8b
RODRIGO\r <jrodrigo.martinez@hotmail.com>
2018-01-05 13:59:14 -0600

    JSON_pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Promise, resolve, reject } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,17 @@ export class AppComponent {
   
   nombre:string = 'Rodrigo';
   arreglo:number [] = [1,2,3,4,5,6,7,8,9,10];
+  pi = Math.PI;
+  a:number = 0.2354;
+  moneda:number = 123.4;
+  persona:any = {
+    nombre: "Rodrigo",
+    clave: 151807,
+    edad: 20,
+    direccion{
+      calle: "Insurgentes",
+      numero: "1678",
+      colonia: "Lindavista"
+    }
+  };
 }
